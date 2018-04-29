0a2663b0c66577dd7cae79af50e43c11f3c2bfec
Sean Kidwell <dev.seankidwell@gmail.com>
2018-04-29 17:46:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  ojbect[key] = value;
+  return object;
+}
