ba8026c61022c300c078cf649ead06503aefedb1
Sean Kidwell <dev.seankidwell@gmail.com>
2018-04-29 17:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var recipes = {};
-function updateObjectWithKeyAndValue(object, Key, value) {
-  return Object.assign({}, objecct, {[key]: value});
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
