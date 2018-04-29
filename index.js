bf54e759a54a6975bf65cee66099a89681875c72
Sean Kidwell <dev.seankidwell@gmail.com>
2018-04-29 17:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone.key;
+  return clone;
+}
