a2492b3fe22c0c204ecf7ed3cdd25300c60a3d6f
Sean Kidwell <dev.seankidwell@gmail.com>
2018-04-29 17:34:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 function updateObjectWithKeyAndValue(object, Key, value) {
-  return Object.assign
+  return Object.assign({}, objecct, )
 }
